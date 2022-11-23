1f0f93c505db273dc2c4ffa2771f7438bfde662d
emefielezn <emefielengozi54@gmail.com>
2022-11-22 19:25:50 -0800

    added readme and scripts for task 0

---
+++ main.h
@@ -0,0 +1,28 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+
+int _printf(const char *format, ...);
+int (*check_specifier(const char*))(va_list);
+/**
+ * struct func - struct for specifier to printer
+ * @t: character to compare
+ * @f: function to handle printing
+ */
+typedef struct func
+{
+		char *t;
+			int (*f)(va_list);
+} func_t;
+
+int print_char(va_list);
+int print_str(va_list);
+int print_cent(va_list);
+int print_int(va_list);
+int print_dec(va_list);
+
+#endif
