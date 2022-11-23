586d3c4e6da8ef384eb17b0efd6320f081d584f5
emefielezn <emefielengozi54@gmail.com>
2022-11-23 00:44:18 -0800

    changes to scripts for task 1 to 4

--- _printf.c
+++ _printf.c
@@ -15,7 +15,13 @@ int _printf(const char *format, ...)
 		{"%", print_percent},
 		{"d", print_integer},
 		{"i", unsigned_integer},
-		{"b", print_binary},		
+		{"b", print_binary},
+		{"r", print_reversed},
+		{"R", rot13},
+		{"u", unsigned_integer},
+		{"o", print_octal},
+		{"x", print_hex},
+		{"X", print_heX},		
 		{NULL, NULL}
 	};
 
